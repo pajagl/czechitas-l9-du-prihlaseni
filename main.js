18dd6348a89827698f42ba2a43cef75d0b2fe752
Pavlina Glozygova <pglozygova@gmail.com>
2023-03-23 18:33:42 +0100

    modified:   index.html modified:   main.js

--- main.js
+++ main.js
@@ -1,28 +1,5 @@
 // tady je místo pro náš program
 
-// Auto
-const auto = document.querySelector('#auto');
-
-const move = (event) => {
-    const tlacitko = event.key;
-
-    if(tlacitko === 'ArrowRight') {
-        auto.style.transform = 'rotateY(0deg)';
-        auto.style.animation = 'to-right 1s forwards';
-    }
-
-    if(tlacitko === 'ArrowLeft') {
-        auto.style.transform = 'rotateY(180deg)';
-        auto.style.animation = 'to-left 1s forwards';
-    }
-  
-    //console.log(event);
-    console.log('keydown');
-    console.log(tlacitko);
-}
-
-window.addEventListener('keydown', move);
-
 // Přihlášení 
 const user = {
 	email: 'bartolomej.rozumbrada@gmail.com',
@@ -49,16 +26,4 @@ document.querySelector('#login-email').addEventListener('submit', (e) => {
 	}
 })
 
-
-// Newsletter
-const formular = document.querySelector('form')
-
-const odebirat = (event) => {
-	event.preventDefault()
-	const input = document.querySelector('input')
-	const email = input.value
-	formular.textContent = `Děkujeme za váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese ${email}.`
-}
-
-formular.addEventListener('submit', odebirat)
 
